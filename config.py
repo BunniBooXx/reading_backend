83a002198ce9beadd085b3671c16bb665c20000e
BunniBooXx <140341233+BunniBooXx@users.noreply.github.com>
2024-01-13 20:16:19 -0500

    second commit

--- config.py
+++ config.py
@@ -3,7 +3,9 @@ import os
 class Config:
     FLASK_APP= os.environ.get("FLASK_APP")
     FLASK_DEBUG= os.environ.get("FLASK_DEBUG")
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    # "postgres://reading_backend_user:WYJ2Tx2B6rtwqbfEYQOYKXxOrhnUIvcG@dpg-cmhikj6n7f5s739t5rjg-a.oregon-postgres.render.com/readingbackend"
+    
     SQLALCHEMY_TRACK_MODIFICATIONS= False
 
     SECRET_KEY = os.environ.get("SECRET_KEY")
